84f47508fde8cb8df82da3737e97ab5724d946b0
Henryford52 <henryuzzi75@gmail.com>
2023-12-29 15:40:03 +0100

    first commit

--- src/components/body/contact/index.jsx
+++ src/components/body/contact/index.jsx
@@ -9,9 +9,9 @@ const Contact = () => {
   return (
     <div className='Contact' data-aos='zoom-out' data-aos-easing= 'ease-out-cubic' data-aos-duration = '2000'>
       <h2>Want to learn more? Get In Touch</h2>
-      <input type="text" placeholder='Full Name'/>
-      <input type="text" placeholder='Email Address'/>
-      <textarea name="" id="" cols="80" rows="0" placeholder='Message'></textarea>
+      <input type="text" placeholder='Full Name' className='input'/>
+      <input type="text" placeholder='Email Address' className='input'/>
+      <textarea name="" id="" cols="80" rows="0" placeholder='Message' className='input'></textarea>
       <button className='btn'>Join the waiting List Now</button>
     </div>
   )
